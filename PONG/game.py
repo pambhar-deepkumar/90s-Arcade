281368c9bcfb2da7f62f31233dfc6e4643598362
Deepkumar Pambhar <pambhardeepkumar@gmail.com>
2022-06-24 10:55:18 +0200

    final v1: Added dynamic speed to the ball and added scoreboard to the screen.

--- PONG/game.py
+++ PONG/game.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from paddle import Paddle
 from pongball import Pongball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,33 +11,38 @@ screen.setup(height=600, width=800)
 screen.title("PONG")
 screen.tracer(0)
 
-lpaddle = Paddle(-350, 0)
-rpaddle = Paddle(350, 0)
+scoreboard = Scoreboard()
+l_paddle = Paddle(-350, 0)
+r_paddle = Paddle(350, 0)
 
 ball = Pongball()
 
 screen.listen()
-screen.onkey(rpaddle.move_up, "Up")
-screen.onkey(rpaddle.move_down, "Down")
-screen.onkey(lpaddle.move_up, "w")
-screen.onkey(lpaddle.move_down, "s")
+screen.onkeypress(r_paddle.move_up, "Up")
+screen.onkeypress(r_paddle.move_down, "Down")
+screen.onkeypress(l_paddle.move_up, "w")
+screen.onkeypress(l_paddle.move_down, "s")
 
 game_is_on = 1
 while game_is_on:
     screen.update()
     ball.move()
-    time.sleep(0.05)
+    time.sleep(ball.ball_speed)
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    if ball.distance(rpaddle) < 50 and ball.xcor() > 320 or ball.distance(lpaddle) < 50 and ball.xcor() < -320:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
     if ball.xcor() > 380:
+        time_sleep = 0.1
         ball.ball_reset()
+        scoreboard.l_update()
 
     if ball.xcor() < -380:
+        time_sleep = 0.1
         ball.ball_reset()
+        scoreboard.r_update()
 
 screen.exitonclick()
--- PONG/pongball.py
+++ PONG/pongball.py
@@ -10,16 +10,19 @@ class Pongball(Turtle):
         self.penup()
         self.x_increment = 10
         self.y_increment = 10
+        self.ball_speed = 0.1
 
     def move(self):
         self.goto(self.xcor() + self.x_increment, self.ycor() + self.y_increment)
 
     def bounce_x(self):
         self.x_increment *= -1
+        self.ball_speed *= 0.9
 
     def bounce_y(self):
         self.y_increment *= -1
 
     def ball_reset(self):
+        self.ball_speed = 0.1
         self.goto(0, 0)
         self.bounce_x()
