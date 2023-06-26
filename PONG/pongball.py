b2e8103f05aa42613c5ef9eb263841eb85b45e89
Deepkumar Pambhar <91485737+pambhar-deepkumar@users.noreply.github.com>
2023-06-26 15:31:21 +0200

    Testing sonarqube

--- PONG/pongball.py
+++ PONG/pongball.py
@@ -18,10 +18,6 @@ class Pongball(Turtle):
     def bounce_x(self):
         self.x_increment *= -1
         self.ball_speed *= 0.9
-
-    def bounce_x(self):
-        self.x_increment *= -1
-        self.ball_speed *= 0.9
         
     def bounce_y(self):
         self.y_increment *= -1
@@ -30,6 +26,11 @@ class Pongball(Turtle):
         self.ball_speed = 0.1
         self.goto(0, 0)
         self.bounce_x()
+
+    def testing_sonar_qube(self):
+        print("Print something!")
+        pass
+        
 
 if __name__=="__main__":
     secret_key = "qkwjfch_123"
